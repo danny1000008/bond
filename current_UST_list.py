a0d2dc68ca93ed3c217f841ceb02bcfac8fcf5dc
danny1000008 <30295639+danny1000008@users.noreply.github.com>
2018-04-25 15:24:51 -0500

    Add files via upload
    Refactored to get rid of multiple if stmts

--- current_UST_list.py
+++ current_UST_list.py
@@ -2,26 +2,22 @@
 # written by Danny Wagstaff 2/2018
 
 from flask import Flask, render_template, request
-from . import contractBasket
+#from . import contractBasketBeta # works with development server (127.0.0.1:5000)
+import contractBasket # works in production (dwagstaff.com/bondapp2)
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods = ['POST'])
 def index_post():
-    FC=request.form['futCon']
-    listTsyData=contractBasket.Basket()
-    if FC=='TU':
-        listTsyData.getTUBasket()
-    elif FC=='FV':
-        listTsyData.getFVBasket()
-    else: # FC='TY'
-        listTsyData.getTYBasket()
-    return render_template('CTDList.html',listData=listTsyData.value, FC=FC,mat=listTsyData.getConAbbr())
+    FC = request.form['futCon'] # futures contract chosen
+    listTsyData = contractBasket.Basket() # class Basket will hold UST securities list
+    listTsyData.getBasket(FC)
+    return render_template('CTDList.html', listData=listTsyData.value, FC = FC, mat = listTsyData.getConAbbr())
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
