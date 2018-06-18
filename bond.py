1b1261a1280f5d192e0012e7e0dc5e42e4e04f96
Danny Wagstaff <danny1000008@yahoo.com>
2018-06-18 09:44:10 -0500

    added price files
    added last week's price files

--- bond.py
+++ bond.py
@@ -4,17 +4,17 @@ from datetime import date, timedelta
 
 # Can we rename this class name to follow Python conventions?
 class bond:
-    def __init__(self,matDate,stlDate,cpn=None,cpnFreq=None):
-        self.matDate=matDate
-        self.stlDate=stlDate
-        self.stlDate=self._getWeekday()
-        self.cpn=cpn
-        self.cpnFreq=cpnFreq
+    def __init__(self, matDate, stlDate, cpn = None, cpnFreq = None):
+        self.matDate = matDate
+        self.stlDate = stlDate
+        self.stlDate = self._getWeekday()
+        self.cpn = cpn
+        self.cpnFreq = cpnFreq
 
 
-    def _incRange(self,start,stop,step=1):
+    def _incRange(self, start, stop, step = 1):
         # an iterator function that iterates from start to stop, inclusive
-        i=start
+        i = start
         while i <= stop:
             yield i
             i += step
@@ -25,73 +25,48 @@ class bond:
         Checks if a date is on a weekend. If it is, it returns the previous weekday; otherwise
         it returns the original date
         '''
-        months={1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
-        if self.stlDate.isoweekday()==6: # 6 = Saturday
+        months={1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May',
+            6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October',
+            11: 'November', 12: 'December'}
+        if self.stlDate.isoweekday() == 6: # 6 = Saturday
             print('Changed settle date from Saturday {0} {1}, {2} to Friday {3} {4}, {5}'
                 .format(months[self.stlDate.month],self.stlDate.day,self.stlDate.year,
                 months[(self.stlDate-timedelta(1)).month],(self.stlDate-timedelta(1)).day,
                 (self.stlDate-timedelta(1)).year))
-            self.stlDate=self.stlDate-timedelta(1)
-        elif self.stlDate.isoweekday()==7: # 7 = Sunday
+            self.stlDate = self.stlDate - timedelta(1)
+        elif self.stlDate.isoweekday() == 7: # 7 = Sunday
             print('Changed settle date from Sunday {0} {1}, {2} to Friday {3} {4}, {5}'
                 .format(months[self.stlDate.month],self.stlDate.day,self.stlDate.year,
                 months[(self.stlDate-timedelta(2)).month],(self.stlDate-timedelta(2)).day,
                 (self.stlDate-timedelta(2)).year))
-            self.stlDate=self.stlDate-timedelta(2)
+            self.stlDate = self.stlDate - timedelta(2)
         return self.stlDate
 
-    def byld(self,price):
-        basis='A/A' # actual/actual day count. In the future I will add other day count options
-        numCpns=self.numCoupons(basis)
-        nextCpnDate=self.nextCpn(self.stlDate)
-        daysBetwCpns=(nextCpnDate-self.prevCpn(self.stlDate)).days
-        daysStlToCpn=(nextCpnDate-self.stlDate).days
-        yearFrac=daysStlToCpn/daysBetwCpns
-        # initial guess for yield
-        r_new = self.cpn
-        P_new=price
-        P_init=price
-        # do 10 iterations of Newton's Method to approximate yield to maturity
-        for i in range(10):
-            r_old=r_new
-            P_new=0
-            dP_new=0
-            for j in self._incRange(1,numCpns):
-                P_new=P_new + (1+r_old/2)**(1-j-yearFrac)
-                dP_new=dP_new+(1-j-yearFrac) / 2 * (1+r_old/2)**(-j-yearFrac)
-            P_new=P_new*50*self.cpn # same as 100 * cpn / 2
-            P_new=P_new+100*(1+r_old/2)**(1-numCpns-yearFrac)
-            P_new=P_new-self.bai(self.cpn,self.matDate,self.stlDate)
-            delta_F=P_new-P_init
-            dP_new=dP_new*50*self.cpn # same as 100 * cpn / 2
-            dP_new=dP_new+50*(1-numCpns-yearFrac)*(1+r_old/2)**(-numCpns-yearFrac)
-            r_new=r_old-delta_F/dP_new
-        return r_new
-
-    def bprice(self,yld):
+    def bprice(self, yld):
         ''' Calculates the price of a bond from its yield to maturity
         '''
-        tempPrice=0.0
-        cpnDate=self.prevCpn(self.stlDate)
-        while cpnDate<self.matDate:
+        tempPrice = 0.0
+        cpnDate = self.prevCpn(self.stlDate)
+        while cpnDate < self.matDate:
             cpnDate = self.nextCpn(cpnDate)
-            DF_Time=(cpnDate-self.stlDate).days
-            PV_Cpn=self.cpn/self.cpnFreq*100/((1+yld/self.cpnFreq)**(self.cpnFreq*DF_Time/365))
-            tempPrice=tempPrice+PV_Cpn
-        PV_Par=100/((1+yld/self.cpnFreq)**(self.cpnFreq*DF_Time/365))
+            DF_Time = (cpnDate - self.stlDate).days
+            PV_Cpn = self.cpn / self.cpnFreq * 100 / ((1 + yld / self.cpnFreq)**
+                (self.cpnFreq * DF_Time / 365))
+            tempPrice = tempPrice + PV_Cpn
+        PV_Par = 100 / ((1 + yld / self.cpnFreq) ** (self.cpnFreq * DF_Time / 365))
         tempPrice = tempPrice + PV_Par-self.bai(self.cpn, self.matDate, self.stlDate)
         return tempPrice
 
-    def dv01(self,yld):
+    def dv01(self, yld):
         '''
         The dv01 function calculates how much changing the bond yield by 1 basis point (0.01%) changes
         the bond price. We average the price move for an increase in yield and a decrease in yield in
         order to lessen the impact of the bond's convexity on dv01.
         '''
-        bPx=self.bprice(yld)
-        bPxBumpUp=self.bprice(yld+0.0001)
-        bPxBumpDown=self.bprice(yld-0.0001)
-        return (abs(bPx-bPxBumpUp)+abs(bPx-bPxBumpDown))/2
+        bPx = self.bprice(yld)
+        bPxBumpUp = self.bprice(yld + 0.0001)
+        bPxBumpDown = self.bprice(yld - 0.0001)
+        return (abs(bPx - bPxBumpUp) + abs(bPx - bPxBumpDown)) / 2
 
     def nextCpn(self, stlDt):
         '''
@@ -110,7 +85,6 @@ class bond:
         '''
         Returns date of previous coupon payment before stlDt
         '''
-
         cpnNext = self.nextCpn(stlDt)
         cpnPrev = date(cpnNext.year - 1, cpnNext.month, cpnNext.day)
         return self.nextCpn(cpnPrev)
@@ -146,7 +120,7 @@ class bond:
         dtPrevCpn = self.prevCpn(self.stlDate)
         daysInPer = (self.nextCpn(self.stlDate) - dtPrevCpn).days
         daysInt = (self.stlDate - dtPrevCpn).days
-        bai = self.cpn / 2 * (daysInt / daysInPer) * 100
+        bai = self.cpn / 2 * (daysInt / (1.0 * daysInPer)) * 100
         return bai
 
     def byld(self, price):
@@ -159,7 +133,7 @@ class bond:
         nextCpnDate = self.nextCpn(self.stlDate)
         daysBetwCpns = (nextCpnDate - prevCpnDate).days
         daysStlToCpn = (nextCpnDate - self.stlDate).days
-        yearFrac = daysStlToCpn / daysBetwCpns
+        yearFrac = daysStlToCpn / (1.0 * daysBetwCpns)
         # initial guess for yield
         r_new = self.cpn
         P_new = price
@@ -173,7 +147,8 @@ class bond:
             dP_new = 0
             for j in self._incRange(1, numCpns):
                 P_new = P_new + (1 + r_old / 2.0) ** (1 - j - yearFrac)
-                dP_new = dP_new + (1 - j - yearFrac) / 2.0 * (1 + r_old / 2.0) ** (-j - yearFrac)
+                dP_new = dP_new + (1 - j - yearFrac) / 2.0 * (1 + r_old / 2.0) ** \
+                    (-j - yearFrac)
             P_new = P_new * 50 * self.cpn # same as 100 * cpn / 2
             P_new = P_new + 100 * (1 + r_old / 2.0) ** (1 - numCpns - yearFrac)
             P_new = P_new - bai
@@ -181,7 +156,8 @@ class bond:
             delta_F = P_new - P_init
 
             dP_new = dP_new * 50 * self.cpn # same as 100 * cpn / 2
-            dP_new = dP_new + 50 * (1 - numCpns - yearFrac) * (1 + r_old / 2.0) ** (-numCpns - yearFrac)
+            dP_new = dP_new + 50 * (1 - numCpns - yearFrac) * (1 + r_old / 2.0) ** \
+                (-numCpns - yearFrac)
             r_new = r_old - delta_F / dP_new
         return round(r_new * 100, 3)
 
