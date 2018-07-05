acf1fc75d56c32e295a2219ba5d774a21968efc7
Danny Wagstaff <danny1000008@yahoo.com>
2018-07-05 14:50:41 -0500

    snake_cased variables
    made variables snake case; moved unused main function to test_bond.py

--- bond.py
+++ bond.py
@@ -4,15 +4,15 @@ from datetime import date, timedelta
 
 # Can we rename this class name to follow Python conventions?
 class bond:
-    def __init__(self, matDate, stlDate, cpn = None, cpnFreq = None):
-        self.matDate = matDate
-        self.stlDate = stlDate
-        self.stlDate = self._getWeekday()
-        self.cpn = cpn
-        self.cpnFreq = cpnFreq
+    def __init__(self, maturity_date, settle_date, coupon = None, coupon_frequency = None):
+        self.maturity_date = maturity_date
+        self.settle_date = settle_date
+        self.settle_date = self._get_weekday()
+        self.coupon = coupon
+        self.coupon_frequency = coupon_frequency
 
 
-    def _incRange(self, start, stop, step = 1):
+    def _inc_range(self, start, stop, step = 1):
         # an iterator function that iterates from start to stop, inclusive
         i = start
         while i <= stop:
@@ -20,7 +20,7 @@ class bond:
             i += step
 
 
-    def _getWeekday(self):
+    def _get_weekday(self):
         '''
         Checks if a date is on a weekend. If it is, it returns the previous weekday; otherwise
         it returns the original date
@@ -28,191 +28,133 @@ class bond:
         months={1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May',
             6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October',
             11: 'November', 12: 'December'}
-        if self.stlDate.isoweekday() == 6: # 6 = Saturday
+        if self.settle_date.isoweekday() == 6: # 6 = Saturday
             print('Changed settle date from Saturday {0} {1}, {2} to Friday {3} {4}, {5}'
-                .format(months[self.stlDate.month],self.stlDate.day,self.stlDate.year,
-                months[(self.stlDate-timedelta(1)).month],(self.stlDate-timedelta(1)).day,
-                (self.stlDate-timedelta(1)).year))
-            self.stlDate = self.stlDate - timedelta(1)
-        elif self.stlDate.isoweekday() == 7: # 7 = Sunday
+                .format(months[self.settle_date.month],self.settle_date.day,self.settle_date.year,
+                months[(self.settle_date-timedelta(1)).month],(self.settle_date-timedelta(1)).day,
+                (self.settle_date-timedelta(1)).year))
+            self.settle_date = self.settle_date - timedelta(1)
+        elif self.settle_date.isoweekday() == 7: # 7 = Sunday
             print('Changed settle date from Sunday {0} {1}, {2} to Friday {3} {4}, {5}'
-                .format(months[self.stlDate.month],self.stlDate.day,self.stlDate.year,
-                months[(self.stlDate-timedelta(2)).month],(self.stlDate-timedelta(2)).day,
-                (self.stlDate-timedelta(2)).year))
-            self.stlDate = self.stlDate - timedelta(2)
-        return self.stlDate
+                .format(months[self.settle_date.month],self.settle_date.day,self.settle_date.year,
+                months[(self.settle_date-timedelta(2)).month],(self.settle_date-timedelta(2)).day,
+                (self.settle_date-timedelta(2)).year))
+            self.settle_date = self.settle_date - timedelta(2)
+        return self.settle_date
 
-    def bprice(self, yld):
+    def bond_price(self, bond_yield):
         ''' Calculates the price of a bond from its yield to maturity
         '''
-        tempPrice = 0.0
-        cpnDate = self.prevCpn(self.stlDate)
-        while cpnDate < self.matDate:
-            cpnDate = self.nextCpn(cpnDate)
-            DF_Time = (cpnDate - self.stlDate).days
-            PV_Cpn = self.cpn / self.cpnFreq * 100 / ((1 + yld / self.cpnFreq)**
-                (self.cpnFreq * DF_Time / 365))
-            tempPrice = tempPrice + PV_Cpn
-        PV_Par = 100 / ((1 + yld / self.cpnFreq) ** (self.cpnFreq * DF_Time / 365))
-        tempPrice = tempPrice + PV_Par-self.bai(self.cpn, self.matDate, self.stlDate)
-        return tempPrice
+        temp_price = 0.0
+        coupon_date = self.previous_coupon(self.settle_date)
+        while coupon_date < self.maturity_date:
+            coupon_date = self.next_coupon(coupon_date)
+            discount_factor_time = (coupon_date - self.settle_date).days
+            present_value_coupon = self.coupon / self.coupon_frequency * 100 / ((1 + bond_yield / self.coupon_frequency)**
+                (self.coupon_frequency * discount_factor_time / 365))
+            temp_price = temp_price + present_value_coupon
+        present_value_par = 100 / ((1 + bond_yield / self.coupon_frequency) ** (self.coupon_frequency * discount_factor_time / 365))
+        temp_price = temp_price + present_value_par-self.accrued_interest(self.coupon, self.maturity_date, self.settle_date)
+        return temp_price
 
-    def dv01(self, yld):
+    def dv01(self, bond_yield):
         '''
         The dv01 function calculates how much changing the bond yield by 1 basis point (0.01%) changes
         the bond price. We average the price move for an increase in yield and a decrease in yield in
         order to lessen the impact of the bond's convexity on dv01.
         '''
-        bPx = self.bprice(yld)
-        bPxBumpUp = self.bprice(yld + 0.0001)
-        bPxBumpDown = self.bprice(yld - 0.0001)
-        return (abs(bPx - bPxBumpUp) + abs(bPx - bPxBumpDown)) / 2
+        bond_price = self.bond_price(bond_yield)
+        bond_price_bump_up = self.bond_price(bond_yield + 0.0001)
+        bond_price_bump_down = self.bond_price(bond_yield - 0.0001)
+        return (abs(bond_price - bond_price_bump_up) + abs(bond_price - bond_price_bump_down)) / 2
 
-    def nextCpn(self, stlDt):
+    def next_coupon(self, settle_date):
         '''
-        Returns date of next coupon payment after stlDt
+        Returns date of next coupon payment after settle_date
         '''
-        newDt = date(self.matDate.year, self.matDate.month, self.matDate.day)
-        cpnNPlus1 = newDt
-        while newDt > stlDt:
-            newDt -= timedelta(days = 185)
-            newDt = date(newDt.year, newDt.month, self.getCpnDayOfMo(newDt.month))
-        newDt += timedelta(days = 180)
-        newDt = date(newDt.year, newDt.month, self.getCpnDayOfMo(newDt.month))
-        return newDt
+        new_date = date(self.maturity_date.year, self.maturity_date.month, self.maturity_date.day)
+        #cpnNPlus1 = new_date;
+        while new_date > settle_date:
+            new_date -= timedelta(days = 185)
+            new_date = date(new_date.year, new_date.month, self.get_coupon_day_of_month(new_date.month))
+        new_date += timedelta(days = 180)
+        new_date = date(new_date.year, new_date.month, self.get_coupon_day_of_month(new_date.month))
+        return new_date
 
-    def prevCpn(self, stlDt):
+    def previous_coupon(self, settle_date):
         '''
-        Returns date of previous coupon payment before stlDt
+        Returns date of previous coupon payment before settle_date
         '''
-        cpnNext = self.nextCpn(stlDt)
-        cpnPrev = date(cpnNext.year - 1, cpnNext.month, cpnNext.day)
-        return self.nextCpn(cpnPrev)
+        bond_coupon_next = self.next_coupon(settle_date)
+        coupon_previous = date(bond_coupon_next.year - 1, bond_coupon_next.month, bond_coupon_next.day)
+        return self.next_coupon(coupon_previous)
 
-    def numCoupons(self, basis = 'A/A'):
+    def get_number_of_coupons(self, basis = 'A/A'):
         '''
-        Returns number of coupons remaining after self.stlDate
+        Returns number of coupons remaining after self.settle_date
         '''
         count = 0
-        cpnNext = self.nextCpn(self.stlDate)
-        while cpnNext < self.matDate:
+        bond_coupon_next = self.next_coupon(self.settle_date)
+        while bond_coupon_next < self.maturity_date:
             count += 1
-            cpnNext = self.nextCpn(cpnNext + timedelta(1))
+            bond_coupon_next = self.next_coupon(bond_coupon_next + timedelta(1))
         #print('count=', count + 1)
         return count + 1
 
-    def getCpnDayOfMo(self, mo):
+    def get_coupon_day_of_month(self, month):
         '''
         Returns the day of the month that a coupon payment will be made
         (typically the 15th or the last day of the month)
         '''
-        daysInMo = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31,
+        days_in_month = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31,
             9: 30, 10: 31, 11: 30, 12: 31}
-        guess1 = daysInMo[mo]
-        if self.matDate.day < 28:
-            guess1 = self.matDate.day
+        guess1 = days_in_month[month]
+        if self.maturity_date.day < 28:
+            guess1 = self.maturity_date.day
         return guess1
 
-    def bai(self):
+    def accrued_interest(self):
         '''
-        Returns the accrued interest from the last coupon payment till self.stlDate
+        Returns the accrued interest from the last coupon payment till self.settle_date
         '''
-        dtPrevCpn = self.prevCpn(self.stlDate)
-        daysInPer = (self.nextCpn(self.stlDate) - dtPrevCpn).days
-        daysInt = (self.stlDate - dtPrevCpn).days
-        bai = self.cpn / 2 * (daysInt / (1.0 * daysInPer)) * 100
-        return bai
+        date_of_prev_coupon = self.previous_coupon(self.settle_date)
+        days_in_period = (self.next_coupon(self.settle_date) - date_of_prev_coupon).days
+        days_of_interest = (self.settle_date - date_of_prev_coupon).days
+        interest = self.coupon / 2 * (days_of_interest / (1.0 * days_in_period)) * 100
+        return interest
 
     def byld(self, price):
         '''
         Returns the yield to maturity of the bond, given a price
         '''
         basis = 'A/A' # actual/actual day count. In the future I will add other day count options
-        numCpns = self.numCoupons(basis)
-        prevCpnDate = self.prevCpn(self.stlDate)
-        nextCpnDate = self.nextCpn(self.stlDate)
-        daysBetwCpns = (nextCpnDate - prevCpnDate).days
-        daysStlToCpn = (nextCpnDate - self.stlDate).days
-        yearFrac = daysStlToCpn / (1.0 * daysBetwCpns)
+        number_of_coupons = self.get_number_of_coupons(basis)
+        prev_coupon_date = self.previous_coupon(self.settle_date)
+        next_coupon_date = self.next_coupon(self.settle_date)
+        days_between_coupons = (next_coupon_date - prev_coupon_date).days
+        settle_date_to_coupon_date = (next_coupon_date - self.settle_date).days
+        year_fraction = settle_date_to_coupon_date / (1.0 * days_between_coupons)
         # initial guess for yield
-        r_new = self.cpn
+        r_new = self.coupon
         P_new = price
         P_init = price
         # do 10 iterations of Newton's Method to approximate yield to maturity
-        bai = self.bai()
+        interest = self.accrued_interest()
         for i in range(10):
             r_old = r_new
             P_old = P_new
             P_new = 0
             dP_new = 0
-            for j in self._incRange(1, numCpns):
-                P_new = P_new + (1 + r_old / 2.0) ** (1 - j - yearFrac)
-                dP_new = dP_new + (1 - j - yearFrac) / 2.0 * (1 + r_old / 2.0) ** \
-                    (-j - yearFrac)
-            P_new = P_new * 50 * self.cpn # same as 100 * cpn / 2
-            P_new = P_new + 100 * (1 + r_old / 2.0) ** (1 - numCpns - yearFrac)
-            P_new = P_new - bai
-
+            for j in self._inc_range(1, number_of_coupons):
+                P_new = P_new + (1 + r_old / 2.0) ** (1 - j - year_fraction)
+                dP_new = dP_new + (1 - j - year_fraction) / 2.0 * (1 + r_old / 2.0) ** \
+                    (-j - year_fraction)
+            P_new = P_new * 50 * self.coupon # same as 100 * cpn / 2
+            P_new = P_new + 100 * (1 + r_old / 2.0) ** (1 - number_of_coupons - year_fraction)
+            P_new = P_new - interest
             delta_F = P_new - P_init
-
-            dP_new = dP_new * 50 * self.cpn # same as 100 * cpn / 2
-            dP_new = dP_new + 50 * (1 - numCpns - yearFrac) * (1 + r_old / 2.0) ** \
-                (-numCpns - yearFrac)
+            dP_new = dP_new * 50 * self.coupon # same as 100 * cpn / 2
+            dP_new = dP_new + 50 * (1 - number_of_coupons - year_fraction) * (1 + r_old / 2.0) ** \
+                (-number_of_coupons - year_fraction)
             r_new = r_old - delta_F / dP_new
         return round(r_new * 100, 3)
-
-
-def main():
-    stlDate=date(2017,10,4)
-    b1=bond(0.01625,2,date(2019,6,30),stlDate)
-    b2=bond(0.0175,2,date(2021,11,30),stlDate)
-    b3=bond(0.025,2,date(2024,5,15),stlDate)
-    b4=bond(0.0175,2,date(2019,9,30),stlDate)
-    b5=bond(0.01875,2,date(2022,2,28),stlDate)
-    b6=bond(0.02375,2,date(2024,8,15),stlDate)
-    b7=bond(0.0225,2,date(2027,2,15),stlDate)
-    b8=bond(0.02375,2,date(2027,5,15),stlDate)
-    matDates=(date(2017,12,18),date(2018,3,18),date(2018,6,19),date(2018,9,18))
-    matRates=(98.685,98.6,98.54,98.49)
-    print(b1.dfcurve(date(2017,9,11), date(2017,9,12), 0.0136, matDates, matRates))
-    print('b1 previous coupon date',b1.prevCpn(stlDate))
-    print('b1 next coupon date',b1.nextCpn(stlDate))
-    print('b1 number of coupons remaining:',b1.numCoupons('A/A'))
-    print('b1 CF',b1.getCF(date(2017,9,1), 1))
-    print('b1 ai',b1.bai(b1.cpn, b1.matDate, b1.stlDate))
-    print('b1 yield',b1.byld(100))
-    print('b1 price',b1.bprice(0.01625))
-    print('b1 DV01',b1.dv01(0.01424)*10000)
-    print('b2 previous coupon date',b2.prevCpn(stlDate))
-    print('b2 next coupon date',b2.nextCpn(stlDate))
-    print('b2 number of coupons remaining:',b2.numCoupons('A/A'))
-    print('b2 CF',b2.getCF(date(2017,9,1), 1))
-    print('b2 ai',b2.bai(b2.cpn, b2.matDate, b2.stlDate))
-    print('b2 yield',b2.byld(100))
-    print('b3 previous coupon date',b3.prevCpn(stlDate))
-    print('b3 next coupon date',b3.nextCpn(stlDate))
-    print('b3 number of coupons remaining:',b3.numCoupons('A/A'))
-    print('b3 CF',b3.getCF(date(2017,9,1), 2))
-    print('b3 yield',b3.byld(100))
-    print('b4 previous coupon date',b4.prevCpn(stlDate))
-    print('b4 next coupon date',b4.nextCpn(stlDate))
-    print('b4 number of coupons remaining:',b4.numCoupons('A/A'))
-    print('b4 CF',b4.getCF(date(2017,12,1), 1))
-    print('b4 yield',b4.byld(100))
-    print('b5 previous coupon date',b5.prevCpn(stlDate))
-    print('b5 next coupon date',b5.nextCpn(stlDate))
-    print('b5 number of coupons remaining:',b5.numCoupons('A/A'))
-    print('b5 CF',b5.getCF(date(2017,12,1), 1))
-    print('b5 yield',b5.byld(100))
-    print('b6 previous coupon date',b6.prevCpn(stlDate))
-    print('b6 next coupon date',b6.nextCpn(stlDate))
-    print('b6 number of coupons remaining:',b6.numCoupons('A/A'))
-    print('b6 CF',b6.getCF(date(2017,12,1), 2))
-    print('b6 yield',b6.byld(100))
-    print('b7 CF',b7.getCF(date(2017,9,1), 2))
-    print('b7 yield',b7.byld(100))
-    print('b8 CF',b8.getCF(date(2017,12,1), 2))
-    print('b8 yield',b8.byld(100))
-
-if __name__=="__main__":
-    main()
